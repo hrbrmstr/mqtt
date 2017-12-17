a723869b737b55425869a7043bcf49b51fe4afc0
boB Rudis <bob@rud.is>
2017-12-17 13:18:36 -0500

    examples

---
+++ examples/dsl2.R
@@ -0,0 +1,29 @@
+library(mqtt)
+library(purrr)
+library(stringi)
+
+.decode_payload <- function(.x) {
+  .x <- readBin(.x, "character")
+  .x <- stri_match_all_regex(.x, "([[:alpha:]]+):([[:digit:]\\.]+)")[[1]][,2:3]
+  .x <- as.list(setNames(as.numeric(.x[,2]), .x[,1]))
+  .x$timestamp <- as.POSIXct(.x$timestamp/1000, origin="1970-01-01 00:00:00")
+  .x
+}
+
+decode_payload <- purrr::safely(.decode_payload)
+
+# change the id #pls
+mqtt_broker("r_test_1337", "broker.mqttdashboard.com", 1883L) %>%
+  mqtt_silence(c("all")) %>%
+  mqtt_subscribe("sfxrider/+/locations", ~{
+    x <- decode_payload(payload)$result
+    if (!is.null(x)) {
+      cat(crayon::yellow(jsonlite::toJSON(x, auto_unbox=TRUE), "\n", sep=""))
+    }
+  }) %>%
+  mqtt_begin() -> tracker
+
+
+for (i in 1:25) mqtt_loop(tracker, timeout = 1000)
+
+mqtt_end(tracker)
